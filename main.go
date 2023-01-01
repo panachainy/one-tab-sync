f3f229cdf0fcbaffce8e8799a6099a878f29bdc0
Panachai <panachai.ny@gmail.com>
2023-01-01 17:28:31 +0700

    update

--- main.go
+++ main.go
@@ -1,74 +1,64 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "os"
+	"log"
+	"os"
 
-    "github.com/urfave/cli/v2"
-    "context"
-	"net/http"
-
-	"golang.org/x/oauth2"
-	"golang.org/x/oauth2/github"
-	"golang.org/x/oauth2/jwt"
+	"context"
 
 	"github.com/google/go-github/github"
+	"github.com/urfave/cli/v2"
+
+	"golang.org/x/oauth2"
 )
+
 const (
 	// Replace this with your GitHub personal access token.
-	personalAccessToken = "YOUR_TOKEN_HERE"
+	personalAccessToken = "xxxxxxxx"
 	// Replace this with the name of the file you want to create.
 	filename = "example.txt"
-	// Replace this with the contents of the file you want to create.
-	fileContents = "This is an example file."
 )
 
 func main() {
-    app := &cli.App{
-        Name:  "boom",
-        Usage: "make an explosive entrance",
-        Action: func(*cli.Context) error {
-            ss()
+	app := &cli.App{
+		Name:  "boom",
+		Usage: "make an explosive entrance",
+		Action: func(*cli.Context) error {
+			xxx()
 
-            return nil
-        },
-    }
-
-    if err := app.Run(os.Args); err != nil {
-        log.Fatal(err)
-    }
-}
-
-func getOneTabData() (string, error) {
-	// Use the local storage API to retrieve the OneTab data
-	// ...
-    return '',nil;
-}
-
-func ss() {
-	// Set up the OAuth2 client.
-	src := oauth2.StaticTokenSource(
-		&oauth2.Token{AccessToken: personalAccessToken},
-	)
-	httpClient := oauth2.NewClient(context.Background(), src)
-
-	// Set up the GitHub client.
-	client := github.NewClient(httpClient)
-
-	// Create the private gist.
-	gist := &github.Gist{
-		Public:   github.Bool(false),
-		Filename: &filename,
-		Files: map[github.GistFilename]github.GistFile{
-			filename: github.GistFile{Content: &fileContents},
+			return nil
 		},
 	}
-	result, _, err := client.Gists.Create(context.Background(), gist)
-	if err != nil {
-		fmt.Printf("Error creating gist: %v\n", err)
-		return
-	}
 
-	fmt.Printf("Gist created successfully: %v\n", *result.HTMLURL)
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
+}
+
+// /Users/panachai/Library/Application Support/Microsoft Edge/Profile 1/Local Storage/leveldb
+
+func xxx() {
+	// Create a new GitHub client
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: personalAccessToken})
+	tc := oauth2.NewClient(ctx, ts)
+	client := github.NewClient(tc)
+
+	// Create a new Gist
+	gist := &github.Gist{
+		Description: github.String("My Gist"),
+		Public:      github.Bool(true),
+		Files: map[github.GistFilename]github.GistFile{
+			"my_file.txt": {
+				Content: github.String("Hello, World!"),
+			},
+		},
+	}
+	// TODO: work on create gist
+	// result, _, err := client.Gists.Create(ctx, gist)
+	// if err != nil {
+	// 	fmt.Printf("Error creating Gist: %v\n", err)
+	// 	return
+	// }
+	// fmt.Printf("Created Gist %v\n", *result.HTMLURL)
 }
